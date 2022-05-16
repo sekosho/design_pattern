0b8bce8e25673f8faf4a682f7ef325aa30ecb12f
sekosho <sskshogo.10.24@gmail.com>
2022-05-17 03:45:11 +0900

    いい内容

---
+++ python_basic/class/base88_interface_segregation.py
@@ -0,0 +1,63 @@
+# interface segregation
+from abc import ABCMeta, abstractmethod
+
+
+# class Athlete(metaclass=ABCMeta):
+#     @abstractmethod
+#     def swim(self):
+#         pass
+
+#     @abstractmethod
+#     def high_jump(self):
+#         pass
+
+#     @abstractmethod
+#     def long_jump(self):
+#         pass
+
+
+# class Athlete1(Athlete):
+#     def swim(self):
+#         print("I swim")
+
+#     def high_jump(self):
+#         pass
+
+#     def long_jump(self):
+#         pass
+
+
+class Athlete(metaclass=ABCMeta):
+    pass
+
+
+class SwimAthlete(Athlete):
+    @abstractmethod
+    def swim(self):
+        pass
+
+
+class JumpAthlete(Athlete):
+    @abstractmethod
+    def high_jump(self):
+        pass
+
+    @abstractmethod
+    def long_jump(self):
+        pass
+
+
+class Athlete1(SwimAthlete):
+    def swim(self):
+        print("I swim")
+
+
+class Athlete2(SwimAthlete, JumpAthlete):
+    def swim(self):
+        print("I swim")
+
+    def high_jump(self):
+        print("I jump")
+
+    def long_jump(self):
+        print("I long jump")
